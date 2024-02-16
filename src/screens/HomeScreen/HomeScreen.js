71e48e6f4736a7d73f97edd94ead9158d3a25762
Riyad Firdaus <riyadf27@gmail.com>
2024-02-16 12:54:49 +0700

    Fix: Add conditional button rendering

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Image, Text, View } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 
 import GradientButton from "../../components/GradientButton";
 import { styles } from "./HomeScreen.styles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [fullName, setFullName] = useState("");
 
   const goToLogin = () => {
     navigation.replace("Login");
@@ -16,6 +18,22 @@ const HomeScreen = () => {
   const goToRegister = () => {
     navigation.replace("Register");
   };
+  const goToMain = () => {
+    navigation.replace("Main");
+  };
+
+  const handleLogout = async () => {
+    await AsyncStorage.removeItem("fullName");
+    navigation.replace("Login");
+  };
+
+  useEffect(() => {
+    AsyncStorage.getItem("fullName").then((name) => {
+      if (name) {
+        setFullName(name);
+      }
+    });
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -29,12 +47,25 @@ const HomeScreen = () => {
         <Text style={styles.subtitle}>Enjoy Rock, Paper, Scissors, Shoot!</Text>
       </View>
       <View style={styles.buttonContainer}>
-        <GradientButton action={goToLogin} text="Login" color="yellow" />
-        <GradientButton
-          action={goToRegister}
-          text="Register"
-          color="flatgreen"
-        />
+        {fullName ? (
+          <>
+            <GradientButton action={goToMain} text="Play" color="flatgreen" />
+            <GradientButton
+              action={handleLogout}
+              text="Logout"
+              color="yellow"
+            />
+          </>
+        ) : (
+          <>
+            <GradientButton action={goToLogin} text="Login" color="yellow" />
+            <GradientButton
+              action={goToRegister}
+              text="Register"
+              color="flatgreen"
+            />
+          </>
+        )}
       </View>
     </View>
   );
